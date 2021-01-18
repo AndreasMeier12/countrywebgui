70ffc3a4fb4c18c4074e4b66e195dd75c8d8c551
Andreas Meier <andreas.meier@stud.unibas.ch>
2021-01-18 18:39:09 +0100

    Draft search form

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="search-field">
+        <form id="search-form" @submit.prevent="search">
+            <label>Search
+                <input v-model="searchName" type="text"/>
+            </label>
+            <button>Go!</button>
+        </form>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SearchForm",
+        data() {
+            return {
+                searchName: '',
+            }
+        },
+        methods: {
+            async search() {
+                console.log("Sorch name: " + this.searchName);
+                try {
+                    const url = 'https://restcountries.eu/rest/v2/name/' + this.searchName;
+                    const data = await fetch(url);
+                    console.log(data.json())
+                } catch (e) {
+                    console.log(e)
+                }
+
+
+
+            }
+
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
