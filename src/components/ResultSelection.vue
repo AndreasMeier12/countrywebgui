515574559aaca3a297d45952c05f32c896f5f674
Andreas Meier <andreas.meier@stud.unibas.ch>
2021-01-19 16:47:34 +0100

    Show only a selection of many results

--- src/components/ResultSelection.vue
+++ src/components/ResultSelection.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="result-selection">
         <p>{{result.length}} result(s)</p>
-        <button  v-for="country in result" :key="country['name']"><img :src="country['flag']" class="button-flag" :alt="country['name']"/> {{country['name']}}</button>
+        <div v-if="result.length<=3">        <button v-for="country in result" :key="country['name']"><img :src="country['flag']" class="button-flag" :alt="country['name']"/> {{country['name']}}</button>
+        </div>
+        <div v-if="result.length>3">        <button v-for="country in result.slice(0,3)" :key="country['name']"><img :src="country['flag']" class="button-flag" :alt="country['name']"/> {{country['name']}}</button>
+            <button v-for="country in result.slice(3,4)" :key="country['name']"><img :src="country['flag']" class="button-flag-more" alt="more"/> ...</button>
+        </div>
     </div>
 
     
@@ -21,6 +25,14 @@
     .button-flag{
         width: 50px;
         height: auto;
+    }
+
+    .button-flag-more{
+        width: 50px;
+        height: auto;
+        filter: alpha(opacity=40);
+        background-color: #000;
+
     }
 
 </style>
