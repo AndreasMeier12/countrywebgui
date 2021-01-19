b3673ff7d6acf283d8ea5dcb2036b6a9a937deb4
Andreas Meier <andreas.meier@stud.unibas.ch>
2021-01-19 16:51:00 +0100

    Grey out last element

--- src/components/ResultSelection.vue
+++ src/components/ResultSelection.vue
@@ -4,7 +4,7 @@
         <div v-if="result.length<=3">        <button v-for="country in result" :key="country['name']"><img :src="country['flag']" class="button-flag" :alt="country['name']"/> {{country['name']}}</button>
         </div>
         <div v-if="result.length>3">        <button v-for="country in result.slice(0,3)" :key="country['name']"><img :src="country['flag']" class="button-flag" :alt="country['name']"/> {{country['name']}}</button>
-            <button v-for="country in result.slice(3,4)" :key="country['name']"><img :src="country['flag']" class="button-flag-more" alt="more"/> ...</button>
+            <button v-for="country in result.slice(3,4)" :key="country['name']"><img :src="country['flag']" class="grey-image" alt="more"/> ...</button>
         </div>
     </div>
 
@@ -30,8 +30,16 @@
     .button-flag-more{
         width: 50px;
         height: auto;
-        filter: alpha(opacity=40);
-        background-color: #000;
+        background-color: gray;
+        opacity: .2;
+
+    }
+
+    .grey-image{
+        width: 50px;
+        height: auto;
+        background-color: gray;
+        opacity: .2;
 
     }
 
