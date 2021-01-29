c803b79baf63ef82c9b3e4b791237c51bfe5cb80
Andreas Meier <andreas.meier@stud.unibas.ch>
2021-01-29 15:17:55 +0100

    Make table interactivity clearer

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -4,16 +4,20 @@
             <tr class="interactive-row" id="data-pop" v-on:click="selectPop()">
                 <th class="my-table" id="data-pop-left">Population</th>
                 <th class="my-table" id="data-pop-right">{{country['population']}}</th>
+                <th class="my-table"><button>+</button></th>
             </tr>
 
             <tr class="my-table" id="data-continent">
                 <th class="my-table" id="data-continent-left">Region</th>
                 <th class="my-table" id="data-continent-right">{{country['region']}}</th>
+                <th></th>
             </tr>
 
             <tr class="interactive-row" v-on:click="selectArea()" id="data-area" >
                 <th class="my-table" id="data-area-left">Area</th>
                 <th class="my-table" id="data-area-right">{{country['area']}}</th>
+                <th class="my-table"><button>+</button></th>
+
             </tr>
 
 
