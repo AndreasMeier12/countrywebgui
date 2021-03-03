9831b8c4be1aa0042f1f3e9942c17ae3f9687058
Andreas Meier <andreas.meier@stud.unibas.ch>
2021-03-03 07:30:54 +0100

    Show correct plural of results

--- src/components/ResultSelection.vue
+++ src/components/ResultSelection.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="result-selection">
-        <p>{{result.length}} result(s) <button v-on:click="toggleShowSelection" class="small-button">{{hideButtonText()}}</button></p>
+        <p>{{result.length}} {{getResultString()}} <button v-on:click="toggleShowSelection" class="small-button">{{hideButtonText()}}</button></p>
         <div v-if="showAll() && this.showSelection">
             <button v-if="result.length > 3" v-on:click="toggleShowMore" class="button-less">-</button>
             <button v-for="country in result" v-on:click="selectCountry(country['name'])" :key="country['name']"><img
@@ -61,7 +61,14 @@
                 }
                 return "hide";
 
+            },
+            getResultString(){
+                if (this.result.length === 1){
+                    return "result";
+                }
+                return "results";
             }
+
         },
         data() {
             return {
